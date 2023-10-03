2b5b6b3a61040fd06d0fa74007b9786617724d40
ushayadav29 <usha.yadav>
2023-10-03 11:36:50 +0530

    jsx code

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 import React from "react";
-import ReactDOM from "";
+import ReactDOM from "react-dom/client"
 
-const heading=React.createElement("h1",{id:"heading"}, "Namastey");
+// const heading=React.createElement("h1",{id:"heading"}, "Namastey");
+// const root=ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+// const jsxHeading = <h1 className="head">welcome to jsx code</h1>;
+// console.log(jsxHeading);
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(jsxHeading);
+const Title=()=>(
+    <h1 className="head" tabIndex="5">Namaste React using JSX</h1>
+);
+//Functional component
+const HeadingComponent=()=>(
+    <div id="container">
+       {Title()}
+        <Title />
+       
+        <h1 className="heading">Namaste React from functional component</h1>
+    </div>
+);
 const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<HeadingComponent/>);
